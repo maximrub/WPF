c01e10dfe0d3018132bb09c1675b04a81dde1803
Maxim Rubchinsky <maxim@rubchinsky.com>
2017-04-03 19:48:25 +0300

    added OnRegistered

--- Caliburn.Micro.LightInject/Caliburn.Micro.LightInject/LightInjectBootstrapper.cs
+++ Caliburn.Micro.LightInject/Caliburn.Micro.LightInject/LightInjectBootstrapper.cs
@@ -8,56 +8,64 @@ namespace Caliburn.Micro.LightInject
 {
     public abstract class LightInjectBootstrapper : BootstrapperBase
     {
-        protected ServiceContainer m_Container;
-
         protected LightInjectBootstrapper()
         {
             Initialize();
         }
 
+        protected ServiceContainer Container { get; private set; }
+
         protected override sealed void Configure()
         {
-            m_Container = new ServiceContainer();
+            Container = new ServiceContainer();
             IServiceCollection services = new ServiceCollection();
             ConfigureServices(services);
-            Register(m_Container);
-            RegisterViews(m_Container);
-            populateContainer(m_Container, services);
+            Register(Container);
+            RegisterViews(Container);
+            populateContainer(Container, services);
+            OnRegistered(Container);
         }
 
         protected override object GetInstance(Type i_Service, string i_Key)
         {
-            return i_Key == null ? m_Container.GetInstance(i_Service) : m_Container.GetInstance(i_Service, i_Key);
+            return i_Key == null ? Container.GetInstance(i_Service) : Container.GetInstance(i_Service, i_Key);
         }
 
         protected override IEnumerable<object> GetAllInstances(Type i_Service)
         {
-            return m_Container.GetAllInstances(i_Service);
+            return Container.GetAllInstances(i_Service);
         }
 
         protected override void BuildUp(object i_Instance)
         {
-            m_Container.InjectProperties(i_Instance);
+            Container.InjectProperties(i_Instance);
         }
 
         /// <summary>
         /// Configure Framework services
         /// </summary>
-        protected internal virtual void ConfigureServices(IServiceCollection i_Services)
+        protected virtual void ConfigureServices(IServiceCollection i_Services)
         {
         }
 
         /// <summary>
         /// Register Components using LightInject container
         /// </summary>
-        protected internal virtual void Register(ServiceContainer i_Container)
+        protected virtual void Register(ServiceContainer i_Container)
         {
         }
 
         /// <summary>
         /// Register Components using LightInject container
         /// </summary>
-        protected internal virtual void RegisterViews(ServiceContainer i_Container)
+        protected virtual void RegisterViews(ServiceContainer i_Container)
+        {
+        }
+
+        /// <summary>
+        /// Preform actions when the LightInject container is populated
+        /// </summary>
+        protected virtual void OnRegistered(ServiceContainer i_Container)
         {
         }
 
